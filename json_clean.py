d632a64c66deb37e36f3f70fe84dadb9e32ba6b1
NekoRectifier <hyx123250@gmail.com>
2023-03-23 20:28:17 +0800

    add: separate json clean func

---
+++ json_clean.py
@@ -0,0 +1,54 @@
+import os
+import json
+import sys
+
+def main(target):
+    try:
+        if target == type(str):
+            for file in os.walk(target):
+                if str(file).endswith(".json"):
+                    removeUnwantedKeys(os.path.join(target, file))
+        elif target == type(list):
+            for path in list:
+                if os.path.exists(path=path) and str(path).endswith(".json"):
+                    removeUnwantedKeys(path)
+        else:
+            raise ValueError
+    except BaseException:
+        print("Occured")
+
+
+def removeUnwantedKeys(abs_path):
+    with open(abs_path, "r", encoding="utf-8") as file_handle:
+        json_data = json.load(file_handle)
+
+        json_data.pop('imageData')
+        json_data.pop('flags')
+        json_data.pop('imagePath')
+        json_data.pop('version')
+
+        json_data['imgWidth'] = json_data.pop('imageWidth')
+        json_data['imgHeight'] = json_data.pop('imageHeight')
+        json_data['objects'] = json_data.pop('shapes')
+        objs = json_data['objects']
+
+        for obj in objs:
+            obj.pop('flags')
+            obj.pop('group_id')
+            obj.pop('shape_type')
+
+            obj['polygon'] = obj.pop('points')
+            for point in obj['polygon']:
+                point[0] = int(point[0])
+                point[1] = int(point[1])
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print("needs a path arg points to dir of jsons")
+    elif len(sys.argv) == 2:
+        if os.path.exists(sys.argv[1]):
+            print("Processing...")
+            main(sys.argv[1])
+        else:
+            raise FileNotFoundError
