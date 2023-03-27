40875fa48b51621f065837e10809b8962cc269a3
NekoRectifier <hyx123250@gmail.com>
2023-03-27 23:08:18 +0800

    package label_inspect.py

--- label_inspect.py
+++ label_inspect.py
@@ -1,16 +1,33 @@
 import os
 import json
+import sys
 
-item_list = ["car", "building", "motorcycle", "vegetation", "road", "person", "sky", "cone"]
+item_list = ["car", "building", "motorcycle",
+             "vegetation", "road", "person", "sky", "cone"]
 
-for root, dirs, files in os.walk("./testdataset"):
-    for file in files:
-        if str(file).endswith(".json"):
 
-            # with open("./final/gtFine/train/alpha/a.json", 'r', encoding="utf-8") as f:
-              with open(os.path.join(root, file)) as f:
-                json_data = json.load(f)
+def main(path):
+    with open("./output.txt", 'a', encoding='utf-8') as output_f:
+        for root, dirs, files in os.walk(path):
+            for file in files:
+                if str(file).endswith(".json"):
 
-                for object in json_data['shapes']:
-                    if object['label'] not in item_list:
-                        print("problem is %s in file: %s"%(object['label'], file))
+                    with open(os.path.join(root, file), 'r', encoding='utf-8') as f:
+                        json_data = json.load(f)
+
+                        for object in json_data['shapes']:
+                            if object['label'] not in item_list:
+
+                                _str = "Label problem found: %s\nIn %s\n" % (
+                                    object['label'], root + '\\' + file)
+                                print(_str)
+                                output_f.write(_str)
+                    f.close()
+
+    output_f.close()
+
+if __name__ == "__main__":
+    if (len(sys.argv) > 1 and len(sys.argv) < 3):
+        main(sys.argv[1])
+    else:
+        print("Wrong command format\nUse: python label_inspect.py [dir]")
